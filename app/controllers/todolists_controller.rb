33efd71f08a78ae214093804cd9c6f078bbee77c
EC2 Default User <ec2-user@ip-172-31-76-58.ec2.internal>
2021-05-04 03:54:55 +0000

    ruby-on-rails-0504

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -28,9 +28,17 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
+
+  Refile.secret_key = '75daed7cbc8d68ecef123f3414e44879a01da711df8d2b9f52dfc67a88d14befbbc698361a86edec1cff391e613424dc78d634cb53f15435fdfa956453a61d03'
 
 end
